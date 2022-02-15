a125c39de82d9162e5770e8a1cb5b3c98b60913a
Gary Huang <jiazhengsang@gmail.com>
2022-02-15 16:56:12 +0800

    inorder traversal

---
+++ leetcode/binary_tree/94-inorder_traversal.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+// recursion
+ var inorderTraversal = function(root) {
+  const resultList = []
+  const recursiveTraversal = (currentNode, resultList) => {
+      if (!currentNode) return
+      recursiveTraversal(currentNode.left, resultList)
+      resultList.push(currentNode.val)
+      recursiveTraversal(currentNode.right, resultList)
+  }
+  recursiveTraversal(root, resultList)
+  return resultList
+};
+
+// iteration
+var inorderTraversal1 = function(root) {
+  let currentNode = root
+  const resultList = [];
+  const stack = [];
+  while (currentNode || stack.length) {
+      while (currentNode) {
+          stack.push(currentNode);
+          currentNode = currentNode.left;
+      }
+      currentNode = stack.pop();
+      resultList.push(currentNode.val);
+      currentNode = currentNode.right;
+  }
+  return resultList;
+};

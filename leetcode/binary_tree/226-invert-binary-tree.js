dd425136c3fcd0659b8216cab8e295ab7735a637
Gary Huang <jiazhengsang@gmail.com>
2022-02-23 14:53:36 +0800

    add 226 revert binary tree

--- leetcode/binary_tree/226-invert-binary-tree.js
+++ leetcode/binary_tree/226-invert-binary-tree.js
@@ -10,6 +10,35 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
- var invertTree = function(root) {
 
+// recursion
+ var invertTree = function(root) {
+  if (root == null) return null
+
+  // 交换当前结点的左右子结点
+  const tempNode = root.left
+  root.left = root.right
+  root.right = tempNode
+
+  // 分别翻转左右子树
+  invertTree(root.left)
+  invertTree(root.right)
+
+  return root
+};
+
+// iteration
+var invertTree = function(root) {
+  if (root == null) return null
+  const queue = [root]
+  while (queue.length > 0) {
+      const currentNode = queue.shift()
+      if (!currentNode) continue
+      const temp = currentNode.left
+      currentNode.left = currentNode.right
+      currentNode.right = temp
+      queue.push(currentNode.left)
+      queue.push(currentNode.right)
+  }
+  return root
 };
